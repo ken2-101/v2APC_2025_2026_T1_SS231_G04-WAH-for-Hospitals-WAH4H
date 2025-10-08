2898c79c4503e760f7bd54e64d340acb1aa6d7db
Elijah Quibin <epquibin@student.apc.edu.ph>
2025-10-08 04:36:38 +0000

    fixed url base and webhook

--- start-n8n.sh
+++ start-n8n.sh
@@ -6,8 +6,8 @@ docker run -d --name n8n \
   -e N8N_HOST="0.0.0.0" \
   -e N8N_PORT=5678 \
   -e N8N_PROTOCOL="https" \
-  -e N8N_EDITOR_BASE_URL="https://newlymango-5678.app.github.dev" \
-  -e WEBHOOK_URL="https://newlymango-5678.app.github.dev" \
+  -e N8N_EDITOR_BASE_URL="https://obscure-enigma-jjqwr695pjvwfqxjr-5678.app.github.dev" \
+  -e WEBHOOK_URL="https://obscure-enigma-jjqwr695pjvwfqxjr-5678.app.github.dev" \
   --restart unless-stopped \
   docker.n8n.io/n8nio/n8n
 echo "n8n started! Access it at https://obscure-enigma-jjqwr695pjvwfqxjr-5678.app.github.dev
