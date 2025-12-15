a7c214ac063b254f7201a06129ce0f5decee9c23
Judge09 <133473201+Judge09@users.noreply.github.com>
2025-12-15 05:58:39 +0000

    Configure Django to use custom User model

--- wah4h-backend/accounts/models.py
+++ wah4h-backend/accounts/models.py
@@ -1,11 +1,24 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class User(AbstractUser):
-    # Add extra fields here if needed
-    is_patient = models.BooleanField(default=False)
-    is_staff_member = models.BooleanField(default=False)
-    is_doctor = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="accounts_users",  # add this
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="accounts_users_permissions",  # add this
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
 
     def __str__(self):
         return self.username
