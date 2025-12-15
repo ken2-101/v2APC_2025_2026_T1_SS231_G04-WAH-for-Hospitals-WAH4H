e7c2d6e25a55323a6eb84e9a0817022cbecdd0f5
Judge09 <133473201+Judge09@users.noreply.github.com>
2025-12-15 04:36:41 +0000

    removed old backend code to start microservice

--- backend/patients/urls.py
+++
@@ -1,9 +0,0 @@
-# patients/urls.py
-from django.urls import path
-from .views import patient_list, patient_detail
-
-urlpatterns = [
-    path('', patient_list, name='patient-list'),
-    path('<str:pk>', patient_detail, name='patient-detail')
-        # Handles GET and POST at /api/patients/
-]
