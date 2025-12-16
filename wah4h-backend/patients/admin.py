c40529aadd61614726b3de5091b008fe31f93d6f
Judge09 <133473201+Judge09@users.noreply.github.com>
2025-12-16 10:05:12 +0000

    create: admin.py for patients backend

--- wah4h-backend/patients/admin.py
+++ wah4h-backend/patients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('patient_id', 'last_name', 'first_name', 'status', 'admission_date', 'department', 'room')
+    search_fields = ('patient_id', 'last_name', 'first_name', 'mobile_number', 'philhealth_id')
+    list_filter = ('status', 'sex', 'civil_status', 'department')
